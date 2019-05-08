7702f17d61f57dc005773d8fb80c9108aaf15d50
jborza <jborza@gmail.com>
2019-05-08 20:22:54 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,15 @@
+# 6502asm Assembler Language Supp
+
+This extension provides syntax highlighting for use with the (6502asm)[http://www.6502asm.com/] and (Easy 6502)[http://skilldrick.github.io/easy6502] variant of 6502 assembly.
+
+## Features
+
+All 6502 opcodes are supported by the syntax highlighter, plus definitions specific to the 6502asm JavaScript family of assemblers (`define` and `dcb`)
+
+Kind regards to (@tlgkccampbell)[https://github.com/tlgkccampbell] for his work on the (code-ca65)[https://github.com/tlgkccampbell/code-ca65] extension.
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of vscode-6502asm.
---
+++ src/extension.ts
@@ -0,0 +1,21 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as util from 'util';
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	let workspaceRoot = vscode.workspace.rootPath;
+	if (workspaceRoot) {
+		// ...
+	}
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+
+}
